9f79c0133bd0e283a0ee3de358c9990168b4c12a
qasim683 <envoyfacilitation@outlook.com>
2020-09-21 11:39:29 +0500

    hellow from qasim

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -6,10 +6,10 @@
             </div>
 
             <div class="mt-6 text-gray-500">
-                from my side Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
+                Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
                 to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
                 you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-                ecosystem to be a breath of fresh air. We hope you love it.
+                ecosystem to be a breath of fresh air. We hope you love it.hellofrom qasim
             </div>
         </div>
     </div>
